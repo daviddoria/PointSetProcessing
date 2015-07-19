aea699bfe91c8a9e387aff8a58e3b3f4d52747d2
Mikael Brudfors <brudfors@gmail.com>
2015-07-19 18:35:45 +0200

    Added code to normalize normals to vtkPointSetNormalEstimation.

--- vtkPointSetNormalEstimation.cxx
+++ vtkPointSetNormalEstimation.cxx
@@ -89,6 +89,19 @@ int vtkPointSetNormalEstimation::RequestData(vtkInformation *vtkNotUsed(request)
     normalArray->SetTuple( pointId, normal ) ;
     }
 
+  // Normalize normals
+  double normal[3] = {0, 0, 0};
+  double magnitude;
+  for (int i = 0; i < normalArray->GetNumberOfTuples(); ++i)
+  {
+	normalArray->GetTuple(i, normal);
+	magnitude = sqrt(normal[0] * normal[0] + normal[1] * normal[1] + normal[2] * normal[2]);
+	normal[0] = normal[0] / magnitude;
+	normal[1] = normal[1] / magnitude;
+	normal[2] = normal[2] / magnitude;
+	normalArray->SetTuple(i, normal);
+  }
+  
   input->GetPointData()->SetNormals(normalArray);
 
   output->ShallowCopy(input);
